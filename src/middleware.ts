bd4d2da9e4432e0b13d865f9ea19c2884586e4e1
kasimibrhaim <umisak.kasim@gmail.com>
2023-05-17 18:21:02 -0500

    Add auth

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+
+export default authMiddleware({
+  publicRoutes: ["/"]
+});
+
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
---
+++ src/pages/dashbord.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function dashbord() {
+  return (
+    <div>dashbord</div>
+  )
+}
+
+export default dashbord
